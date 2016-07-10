24f91382b085bac724295ada35a49f0527fd87e8
Matt Mazzola <mattmazzola@live.com>
2016-07-10 12:48:05 -0700

    Add dts, docs, and deploy docs to ghpages.

---
+++ CONTRIBUTING.md
@@ -0,0 +1,27 @@
+# Contributing
+
+## Clone repository
+```
+git clone https://github.com/mattmazzola/perceptron.git
+```
+
+## Install global dependencies if needed
+```
+npm install -g gulp typings
+```
+
+## Install local dependencies
+```
+npm install
+typings install
+```
+
+## Build
+```
+gulp build
+```
+
+## Test
+```
+gulp test --chrome
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 var gulp = require('gulp-help')(require('gulp')),
   del = require('del'),
   karma = require('karma'),
+  flatten = require('gulp-flatten'),
+  ghPages = require('gulp-gh-pages'),
   rollup = require('rollup').rollup,
 	rollupTypescript  = require('rollup-plugin-typescript'),
   runSequence = require('run-sequence'),
+  ts = require('gulp-typescript'),
+  typedoc = require('gulp-typedoc'),
   argv = require('yargs').argv
   ;
+
+
 
 
 gulp.task('build', 'Build for release', function (done) {
     return runSequence(
         'clean:dist',
         'compile:src',
+        'compile:dts',
         // 'min',
         // 'header',
         done
@@ -31,6 +38,12 @@ gulp.task('test', 'Run all tests', function (done) {
 gulp.task('clean:dist', 'Clean dist directory', function () {
     return del([
         './dist/**/*'
+    ]);
+});
+
+gulp.task('clean:docs', 'Clean docs directory', function () {
+    return del([
+        './docs/**/*'
     ]);
 });
 
@@ -56,6 +69,20 @@ gulp.task('compile:src', function () {
   });
 });
 
+gulp.task('compile:dts', 'Generate dts files from modules', function () {
+    var tsProject = ts.createProject('tsconfig.json', {
+        declaration: true,
+        sourceMap: false
+    });
+    
+    var tsResult = tsProject.src()
+        .pipe(ts(tsProject));
+    
+    return tsResult.dts
+        .pipe(flatten())
+        .pipe(gulp.dest('./dist'));
+});
+
 gulp.task('compile:spec', 'Compile typescript for tests', function () {
   return rollup({
     entry: 'test/perceptron.spec.ts',
@@ -77,4 +104,25 @@ gulp.task('test:spec', 'Runs spec tests', function (done) {
         singleRun: argv.watch ? false : true,
         captureTimeout: argv.timeout || 20000
     }, done);
+});
+
+gulp.task('docs', 'Generate documentation', function () {
+  return gulp.src(["src/*.ts"])
+    .pipe(typedoc({
+      mode: "modules",
+
+      // Output options (see typedoc docs) 
+      out: "./docs",
+
+      // TypeDoc options (see typedoc docs) 
+      name: "Perceptron"
+    }))
+    .pipe(gulp.dest('./docs'));
+});
+
+gulp.task('ghpages', 'Update ghpages', function ()  {
+  return gulp.src(['docs/**/*'])
+    .pipe(ghPages({
+      force: true
+    }));
 });
